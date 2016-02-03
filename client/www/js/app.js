256e3af1e9b622e270dc2ed81902c37a10a27607
badiniibrahim <sawadogo.badiniibrahim@yahoo.fr>
2016-02-03 10:42:04 +0100

    mise à jour

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -3,105 +3,94 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ngMockE2E'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
 
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
     }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
+    if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+.config(function ($stateProvider, $urlRouterProvider, USER_ROLES) {
   $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginCtrl'
+  })
+  .state('main', {
+    url: '/',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: 'templates/main.html'
   })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
+  .state('main.dash', {
+    url: 'main/dash',
     views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.lists', {
-      url: '/lists',
-      views: {
-        'tab-lists': {
-          templateUrl: 'templates/tab-lists.html',
-          controller: 'ListsCtrl'
+        'dash-tab': {
+          templateUrl: 'templates/dashboard.html',
+          controller: 'DashCtrl'
         }
-      }
-    })
-    .state('tab.list-detail', {
-      url: '/lists/:listId',
-      views: {
-        'tab-lists': {
-          templateUrl: 'templates/list-detail.html',
-          controller: 'ListDetailCtrl'
+    }
+  })
+  .state('main.login', {
+    url: 'main/login',
+    views: {
+        'login-tab': {
+          templateUrl: 'templates/login.html',
+          controller: 'LoginCtrl'
         }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
     }
   })
 
-  .state('tab.create', {
-    url: '/create',
+  .state('main.create', {
+    url: 'main/create',
     views: {
-      'tab-create': {
-        templateUrl: 'templates/tab-create.html',
-        controller: 'CreateCtrl'
-      }
+        'create-tab': {
+          templateUrl: 'templates/create.html',
+          controller: 'CreateCtrl'
+        }
+    }
+  })
+  .state('main.public', {
+    url: 'main/public',
+    views: {
+        'public-tab': {
+          templateUrl: 'templates/public.html'
+        }
     }
   })
 
-  .state('tab.connexion', {
-    url: '/connexion',
+  .state('main.admin', {
+    url: 'main/admin',
     views: {
-      'tab-connexion': {
-        templateUrl: 'templates/tab-connexion.html',
-        controller: 'ConnexionCtrl'
-      }
+        'admin-tab': {
+          templateUrl: 'templates/admin.html'
+        }
+    },
+    data: {
+      authorizedRoles: [USER_ROLES.admin]
     }
   });
+  
+  // Thanks to Ben Noblet!
+  $urlRouterProvider.otherwise(function ($injector, $location) {
+    var $state = $injector.get("$state");
+    $state.go("main.dash");
+  });
+})
 
-
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
+.run(function($httpBackend){
+  $httpBackend.whenGET(/templates\/\w+.*/).passThrough();
+})
