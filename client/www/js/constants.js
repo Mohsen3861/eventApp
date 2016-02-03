756f7a2d864318ce44ad2487b88b4159aca71bbb
badiniibrahim <sawadogo.badiniibrahim@yahoo.fr>
2016-02-03 10:52:06 +0100

    mise à jour 2

---
+++ client/www/js/constants.js
@@ -0,0 +1,12 @@
+angular.module('starter')
+.constant('AUTH_EVENTS', {
+	notAuthentificated:'auth-no-authentificateg',
+	notAuthrozied: 'auth-not-authrozied'
+
+})
+
+.constant('USER_ROLES', {
+	Admin:'admin-roles',
+	Public: 'public'
+
+});
---
+++ client/www/js/controller.js
@@ -0,0 +1,6 @@
+angular.module('starter')
+ 
+.controller('AppCtrl', function() {})
+.controller('LoginCtrl', function() {})
+.controller('CreateCtrl', function() {})
+.controller('DashCtrl', function() {});
